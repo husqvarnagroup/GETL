05f36f4a89f1b6282f9bc8621e15ad1b1916a3ba
Niels Lemmens <draso.odin@gmail.com>
2020-05-28 08:24:04 +0200

    Cleanup mkdocs_plugins.py

--- mkdocs_plugins.py
+++ mkdocs_plugins.py
@@ -1,6 +1,8 @@
 import ast
 from io import StringIO
+from operator import attrgetter
 from pathlib import Path
+from typing import Iterable
 
 from mkdocs.plugins import BasePlugin
 
@@ -8,36 +10,45 @@ from mkdocs.plugins import BasePlugin
 class LiftBlock(BasePlugin):
     config_scheme = ()
 
-    def on_serve(self, server, config, builder):
+    def get_blocks_path(self, config):
         project_path = Path(config["config_file_path"]).parent
-        self.blocks_path = project_path / "getl" / "blocks"
+        return project_path / "getl" / "blocks"
 
-        server.watch(blocks_path, builder)
+    def on_serve(self, server, config, builder):
+        server.watch(self.get_blocks_path(config), builder)
         return server
 
     def on_page_markdown(self, markdown, page, config, files):
         if "<lift-blocks>" in markdown:
-            io = StringIO()
-            write_file(self.blocks_path, io)
-            return markdown.replace("<lift-blocks>", io.getvalue())
+            doc = generate_entrypoint_docs(
+                self.get_blocks_path(config).glob("*/entrypoint.py")
+            )
+            return markdown.replace("<lift-blocks>", doc)
         return markdown
 
 
-def write_file(src: Path, dest_file):
-    for entrypoint_path in sorted(src.glob("*/entrypoint.py")):
+def generate_entrypoint_docs(python_files: Iterable[Path]) -> str:
+    dest_file = StringIO()
+    for entrypoint_path in sorted(python_files, key=attrgetter("parent.name")):
         module_name = entrypoint_path.parent.name
         module = ast.parse(entrypoint_path.read_text())
         module_doc = ast.get_docstring(module)
         dest_file.write(f"## {module_name}\n\n{module_doc}\n\n")
 
-        for node in module.body:
-            if not isinstance(node, ast.FunctionDef):
-                continue
+        function_nodes = sorted(
+            (
+                node
+                for node in module.body
+                if isinstance(node, ast.FunctionDef) and valid_func_name(node.name)
+            ),
+            key=attrgetter("name"),
+        )
+
+        for node in function_nodes:
             func_name = node.name
-            if not valid_func_name(func_name):
-                continue
             func_doc = ast.get_docstring(node)
             dest_file.write(f"### {module_name}::{func_name}\n\n{func_doc}\n\n")
+    return dest_file.getvalue()
 
 
 def valid_func_name(func_name: str) -> bool:
